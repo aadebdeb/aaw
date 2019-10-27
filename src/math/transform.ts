8a2c95b91ef0fd6654c8df2c2115cb26bf017816
aadebdeb <aa.debdeb@gmail.com>
2019-10-27 22:36:19 +0900

    change property name

--- src/math/transform.ts
+++ src/math/transform.ts
@@ -9,7 +9,7 @@ type ConstructorOptions = {
 };
 
 export class Transform {
-  private _mat4: Mat4 | null = null;
+  private _mat: Mat4 | null = null;
   private _translation: Vec3;
   private _rotation: Quaternion;
   private _scaling: Vec3;
@@ -25,7 +25,7 @@ export class Transform {
 
   static identity(): Transform {
     const transform =  new Transform();
-    transform._mat4 = Mat4.identity();
+    transform._mat = Mat4.identity();
     return transform;
   }
 
@@ -39,7 +39,7 @@ export class Transform {
       throw new Error('unsupported combination of args.');
     }
     const transform = new Transform({translation: t});
-    transform._mat4 = Mat4.translate(x, y, z);
+    transform._mat = Mat4.translate(x, y, z);
     return transform;
   }
 
@@ -51,7 +51,7 @@ export class Transform {
       r = new Vec3(x, y, z);
     }
     const transform = new Transform({scaling: r});
-    transform._mat4 = Mat4.scale(x, y, z);
+    transform._mat = Mat4.scale(x, y, z);
     return transform;
   }
 
@@ -81,34 +81,34 @@ export class Transform {
 
   set translation(translation: Vec3) {
     this._translation = translation.clone();
-    this._mat4 = null;
+    this._mat = null;
   }
 
   set rotation(rotation: Quaternion) {
     this._rotation = rotation.clone();
-    this._mat4 = null;
+    this._mat = null;
   }
 
   set scaling(scaling: Vec3) {
     this._scaling = scaling.clone();
-    this._mat4 = null;
+    this._mat = null;
   }
 
   translate(x: number, y: number, z: number): Transform {
     this.translation.add(new Vec3(x, y, z));
-    this._mat4 = null;
+    this._mat = null;
     return this;
   }
 
   scale(x: number, y: number, z: number): Transform {
     this.scaling.mul(new Vec3(x, y, z));
-    this._mat4 = null;
+    this._mat = null;
     return this;
   }
 
   rotate(q: Quaternion): Transform {
     this.rotation.mul(q);
-    this._mat4 = null;
+    this._mat = null;
     return this;
   }
 
@@ -116,18 +116,18 @@ export class Transform {
     const forward = Vec3.sub(target, origin).norm();
     this._translation = origin.clone();
     this._rotation = Quaternion.lookTo(forward, up);
-    this._mat4 = null;
+    this._mat = null;
     return this;
   }
 
-  get mat4(): Mat4 {
-    if (this._mat4 === null) {
-      this._mat4 = Mat4.mul(
+  get mat(): Mat4 {
+    if (this._mat === null) {
+      this._mat = Mat4.mul(
         Mat4.translate(this.translation),
         this._rotation.toMat4(),
         Mat4.scale(this.scaling)
       );
     }
-    return this._mat4;
+    return this._mat;
   }
 }
